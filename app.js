8bb1dc6957f853554e290c4f2d2803d082697af3
nuong dang <dangthinuong1804@gmail.com>
2022-04-09 21:11:31 +0700

    app

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -12,3 +12,15 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('học đi nương ạ .Dốt lắm rồi hehe!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
